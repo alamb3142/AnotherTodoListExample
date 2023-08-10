58254c7752fa6b16b00c901fd09cc2baddcdffe6
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-10 19:00:01 +0100

    Formatting + removed dead code

--- TodoList.Service/Domain/Todos/Todo.cs
+++ TodoList.Service/Domain/Todos/Todo.cs
@@ -7,59 +7,64 @@ namespace Domain.Todos;
 
 public class Todo : Entity, IAggregateRoot
 {
-	public int? TodoListId { get; protected set; }
-	public Title Title { get; protected set; }
-	public bool Completed { get; protected set; }
-	public DateTime? DueUtc { get; protected set; }
+    public int? TodoListId { get; protected set; }
+    public Title Title { get; protected set; }
+    public bool Completed { get; protected set; }
+    public DateTime? DueUtc { get; protected set; }
 
-	private Todo(int id, Title title, bool completed, int? todoListId = null, DateTime? dueUtc = null) : this()
-	{
-		Id = id;
-		Title = title;
-		Completed = completed;
-		TodoListId = todoListId;
-		DueUtc = dueUtc;
-	}
+    private Todo(int id, Title title, bool completed, int? todoListId = null, DateTime? dueUtc = null) : this()
+    {
+        Id = id;
+        Title = title;
+        Completed = completed;
+        TodoListId = todoListId;
+        DueUtc = dueUtc;
+    }
 
-	public static Todo Create(Title title)
-	{
-		return new Todo(default, title, false);
-	}
+    public static Todo Create(Title title)
+    {
+        return new Todo(default, title, false);
+    }
 
-	public void UpdateTitle(Title title)
-	{
-		Title = title;
-	}
+    public static Result<Todo> Create(string title)
+    {
+        var titleResult = Title.Create(title);
+        if (titleResult.IsFailed)
+            return Result.Fail<Todo>(titleResult.Errors);
 
-	public Result Rename(string title)
-	{
-		var titleResult = Title.Create(title);
+        return Result.Ok(new Todo(default, titleResult.Value, false));
+    }
 
-		if (titleResult.IsFailed)
-		{
-			return Result.Fail(titleResult.Errors);
-		}
 
-		return Result.Ok();
-	}
+    public Result Rename(string title)
+    {
+        var titleResult = Title.Create(title);
 
-	public void Complete()
-	{
-		Completed = true;
-	}
+        if (titleResult.IsFailed)
+        {
+            return Result.Fail(titleResult.Errors);
+        }
 
-	public void AddToList(TodoList todoList)
-	{
-		// Todo domain event ?
-		TodoListId = todoList.Id;
-	}
+        return Result.Ok();
+    }
 
-	public void RemoveFromList()
-	{
-		TodoListId = null;
-	}
+    public void Complete()
+    {
+        Completed = true;
+    }
+
+    public void AddToList(TodoList todoList)
+    {
+        // Todo domain event ?
+        TodoListId = todoList.Id;
+    }
+
+    public void RemoveFromList()
+    {
+        TodoListId = null;
+    }
 
 #nullable disable
-	private Todo() { }
+    private Todo() { }
 #nullable enable
 }
