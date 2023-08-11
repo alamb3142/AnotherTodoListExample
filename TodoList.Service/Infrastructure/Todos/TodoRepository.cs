f3dc4d47e9b41e827461bb3f63fc1c3bba29d06a
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-11 13:14:18 +0100

    TodoRepository formatting + NotFoundErrors

--- TodoList.Service/Infrastructure/Todos/TodoRepository.cs
+++ TodoList.Service/Infrastructure/Todos/TodoRepository.cs
@@ -1,5 +1,5 @@
-
 using Domain.Common;
+using Domain.Common.Errors;
 using Domain.Todos;
 using FluentResults;
 using Infrastructure.Common;
@@ -9,50 +9,51 @@ namespace Infrastructure.Todos;
 
 public class TodoRepository : ITodoRepository, IDisposable
 {
-	private readonly TodoListContext _context;
+    private readonly TodoListContext _context;
 
-	IUnitOfWork IRepository<Todo>.UnitOfWork => _context;
+    IUnitOfWork IRepository<Todo>.UnitOfWork => _context;
 
-	public TodoRepository(TodoListContext context)
-	{
-		_context = context;
-	}
+    public TodoRepository(TodoListContext context)
+    {
+        _context = context;
+    }
 
-	public async Task<int> CreateAsync(Todo todo, CancellationToken cancellationToken)
-	{
-		await _context.Todos.AddAsync(todo, cancellationToken);
-		return todo.Id;
-	}
+    public async Task<int> CreateAsync(Todo todo, CancellationToken cancellationToken)
+    {
+        await _context.Todos.AddAsync(todo, cancellationToken);
+        return todo.Id;
+    }
 
-	public Task<List<Todo>> GetAllAsync(CancellationToken cancellationToken)
-	{
-		return _context.Todos.ToListAsync(cancellationToken);
-	}
+    public Task<List<Todo>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return _context.Todos.ToListAsync(cancellationToken);
+    }
 
-	public async Task<Result> DeleteAsync(int todoId, CancellationToken cancellationToken)
-	{
-		var todo = await _context.Todos.FindAsync(todoId, cancellationToken);
+    public async Task<Result> DeleteAsync(int todoId, CancellationToken cancellationToken)
+    {
+        var todo = await _context.Todos.FindAsync(todoId, cancellationToken);
 
-		if (todo is null)
-			return Result.Fail("Todo not found");
+        if (todo is null)
+            return Result.Fail(new NotFoundError(todoId, nameof(Todo)));
 
-		_context.Remove(todo);
+        _context.Remove(todo);
 
-		return Result.Ok();
-	}
+        return Result.Ok();
+    }
 
-	public async Task<Result<Todo>> GetByIdAsync(int id, CancellationToken cancellationToken)
-	{
-		var todo = await _context.Todos.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+    public async Task<Result<Todo>> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        var todo = await _context.Todos.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
 
-		if (todo is null)
-			return Result.Fail("Todo not found");
+        if (todo is null)
+            return Result.Fail(new NotFoundError(id, nameof(Todo)));
 
-		return Result.Ok(todo);
-	}
+        return Result.Ok(todo);
+    }
 
-	public void Dispose()
-	{
-		this._context.SaveChanges();
-	}
+    public void Dispose()
+    {
+        // TODO: Move save logic into a pipeline behaviour
+        // this._context.SaveChanges();
+    }
 }
