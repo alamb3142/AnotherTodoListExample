84b25f5d7306e8690a54261641d2b13de9f837c4
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-11 14:48:19 +0100

    Formatting + more TodoController endpoints

--- TodoList.Service/Api.Http/Controllers/TodoController.cs
+++ TodoList.Service/Api.Http/Controllers/TodoController.cs
@@ -3,7 +3,7 @@ using Application.Todos.CreateTodo;
 using Application.Todos.GetAllTodos;
 using Application.Todos.GetFilteredTodos;
 using Application.Todos.DeleteTodo;
-using Application.Todos.UpdateTodo;
+using Domain.Common.Errors;
 using FluentResults;
 using Mediator;
 using Microsoft.AspNetCore.Mvc;
@@ -15,72 +15,83 @@ namespace Api.Http.Todos;
 [Route("Todos")]
 public class TodoController : ControllerBase
 {
-	private readonly IMediator _mediator;
+    private readonly IMediator _mediator;
 
-	public TodoController(IMediator mediator)
-	{
-		_mediator = mediator;
-	}
+    public TodoController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 
-	[HttpPost]
-	public async Task<ActionResult> RenameTodo(RenameTodoCommand command, CancellationToken cancellationToken)
-	{
-		var result = await _mediator.Send(command);
+    [HttpPost("Rename")]
+    [ProducesResponseType((int)HttpStatusCode.Accepted)]
+    [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
+    public async Task<ActionResult> RenameTodo(
+        RenameTodoCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _mediator.Send(command);
 
-		if (result.IsSuccess)
-		{
-			return Accepted();
-		}
+        if (result.IsSuccess)
+        {
+            return Accepted();
+        }
 
-		if (result.HasError<NotFoundError>())
-		{
-			var error = result.Errors.First(x => x.GetType() == typeof(NotFoundError));
-			return NotFound(error.Message);
-		}
+        if (result.HasError<NotFoundError>())
+        {
+            var error = result.Errors.First(x => x.GetType() == typeof(NotFoundError));
+            return NotFound(error.Message);
+        }
 
-		return Problem(statusCode: (int)HttpStatusCode.InternalServerError);
-	}
+        return Problem(statusCode: (int)HttpStatusCode.InternalServerError);
+    }
 
-	[HttpPost]
-	[ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
-	[ProducesErrorResponseType(typeof(List<IError>))]
-	public async Task<ActionResult<int>> CreateTodo(CreateTodoCommand command, CancellationToken cancellationToken)
-	{
-		var response = await _mediator.Send(command, cancellationToken);
+    [HttpPost]
+    [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+    [ProducesErrorResponseType(typeof(List<IError>))]
+    public async Task<ActionResult<int>> CreateTodo(
+        CreateTodoCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var response = await _mediator.Send(command, cancellationToken);
 
-		if (response.IsFailed)
-			return BadRequest(response.Errors);
+        if (response.IsFailed)
+            return BadRequest(response.Errors);
 
-		return Ok(response.Value);
-	}
+        return Ok(response.Value);
+    }
 
-	[HttpPut]
-	public async Task<ActionResult> UpdateTodo(UpdateTodoCommand command, CancellationToken cancellationToken)
-	{
-		var response = await _mediator.Send(command, cancellationToken);
-		return Accepted();
-	}
+    [HttpDelete]
+    public async Task<ActionResult> DeleteTodo(
+        DeleteTodoCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        await _mediator.Send(command, cancellationToken);
+        return Accepted();
+    }
 
-	[HttpDelete]
-	public async Task<ActionResult> DeleteTodo(DeleteTodoCommand command, CancellationToken cancellationToken)
-	{
-		await _mediator.Send(command, cancellationToken);
-		return Accepted();
-	}
+    [HttpGet]
+    [ProducesResponseType(typeof(GetAllTodosQueryResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<GetAllTodosQueryResponse>> GetAll(
+        GetAllTodosQuery query,
+        CancellationToken cancellationToken
+    )
+    {
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
 
-	[Route("todos")]
-	[ProducesResponseType(typeof(GetAllTodosQueryResponse), (int)HttpStatusCode.OK)]
-	public async Task<ActionResult<GetAllTodosQueryResponse>> GetAll(GetAllTodosQuery query, CancellationToken cancellationToken)
-	{
-		var response = await _mediator.Send(query);
-		return Ok(response);
-	}
-
-	[HttpGet]
-	[ProducesResponseType(typeof(GetFilteredTodosQueryResponse), (int)HttpStatusCode.OK)]
-	public async Task<ActionResult> GetFiltered(GetFilteredTodosQuery query, CancellationToken cancellationToken)
-	{
-		var response = await _mediator.Send(query);
-		return Ok(response);
-	}
+    [HttpGet]
+    [Route("filtered")]
+    [ProducesResponseType(typeof(GetFilteredTodosQueryResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> GetFiltered(
+        GetFilteredTodosQuery query,
+        CancellationToken cancellationToken
+    )
+    {
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
 }
