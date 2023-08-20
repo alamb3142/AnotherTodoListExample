5950c3b236d013c349a0f4f1dd224cb6cd51649f
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-20 22:00:08 +0100

    Adds TodoList DbSet

--- TodoList.Service/Infrastructure/Common/TodoListContext.cs
+++ TodoList.Service/Infrastructure/Common/TodoListContext.cs
@@ -1,22 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Domain.Todos;
 using Domain.Common;
+using Domain.TodoLists;
 
 #nullable disable
 namespace Infrastructure.Common;
 
 public class TodoListContext : DbContext, IUnitOfWork
 {
-	public DbSet<Todo> Todos { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+    public DbSet<TodoList> TodoLists { get; set; }
 
-	public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
-	{
+    public TodoListContext(DbContextOptions<TodoListContext> options)
+        : base(options) { }
 
-	}
-
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-	}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
 }
 #nullable enable
