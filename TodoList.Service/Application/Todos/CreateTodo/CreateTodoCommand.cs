c4002ef94a16724e6bfde51e766b1275bb1b9d87
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-21 19:55:57 +0100

    Refactors CreateTodoCommand

--- TodoList.Service/Application/Todos/CreateTodo/CreateTodoCommand.cs
+++ TodoList.Service/Application/Todos/CreateTodo/CreateTodoCommand.cs
@@ -9,39 +9,32 @@ namespace Application.Todos.CreateTodo;
 
 public class CreateTodoCommand : ICommand<Result>
 {
-    public required string Title { get; init; }
+	public required string Title { get; init; }
 }
 
 public class CreateTodoCommandHandler : ICommandHandler<CreateTodoCommand, Result>
 {
-    private readonly ITodoRepository repository;
+	private readonly ITodoRepository repository;
 
-    public CreateTodoCommandHandler(ITodoRepository todoRepository)
-    {
-        repository = todoRepository;
-    }
+	public CreateTodoCommandHandler(ITodoRepository todoRepository)
+	{
+		repository = todoRepository;
+	}
 
-    public ValueTask<Result> Handle(
-        CreateTodoCommand command,
-        CancellationToken cancellationToken
-    )
-    {
-        Result<Title> titleResult = Title.Create(command.Title);
-        if (titleResult.IsFailed)
+	public ValueTask<Result> Handle(
+		CreateTodoCommand command,
+		CancellationToken cancellationToken
+	)
+	{
+		Result<Title> titleResult = Title.Create(command.Title);
+		if (titleResult.IsFailed)
 			return ValueTask.FromResult(titleResult.ToResult());
 
-        var todo = Todo.Create(titleResult.Value);
-        repository.Create(todo);
+		var todo = Todo.Create(titleResult.Value);
+		repository.Create(todo);
 
 		var result = Result.Ok();
 		return new ValueTask<Result>(result);
-    }
+	}
 }
 
-public class CreateTodoCommandValidator : AbstractValidator<CreateTodoCommand>
-{
-    public CreateTodoCommandValidator()
-    {
-        RuleFor(c => c.Title).NotEmpty();
-    }
-}
