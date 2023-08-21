6e09840009f88cafe49c52714887bda81995f618
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-21 19:56:44 +0100

    Refactors RenameTodoListCommand

--- TodoList.Service/Application/TodoLists/Rename/RenameTodoListCommand.cs
+++ TodoList.Service/Application/TodoLists/Rename/RenameTodoListCommand.cs
@@ -6,31 +6,33 @@ namespace Application.TodoLists.Rename;
 
 public class RenameTodoListCommand : ICommand<Result>
 {
-    public required int Id { get; set; }
-    public required string Name { get; set; }
+	public required int Id { get; set; }
+	public required string Name { get; set; }
 }
 
 public class RenameTodoListCommandHandler : ICommandHandler<RenameTodoListCommand, Result>
 {
-    private readonly ITodoListRepository _repository;
+	private readonly ITodoListRepository _repository;
 
-    public RenameTodoListCommandHandler(ITodoListRepository repository)
-    {
-        _repository = repository;
-    }
+	public RenameTodoListCommandHandler(ITodoListRepository repository)
+	{
+		_repository = repository;
+	}
 
-    public async ValueTask<Result> Handle(RenameTodoListCommand command, CancellationToken cancellationToken)
-    {
-        var todoListResult = await _repository.GetByIdAsync(command.Id, cancellationToken);
-        if (todoListResult.IsFailed)
-            return Result.Fail(todoListResult.Errors);
+	public async ValueTask<Result> Handle(RenameTodoListCommand command, CancellationToken cancellationToken)
+	{
+		var todoListResult = await _repository.GetByIdAsync(command.Id, cancellationToken);
+		if (todoListResult.IsFailed)
+			return Result.Fail(todoListResult.Errors);
 
-        var todoList = todoListResult.Value;
-        var renameResult = todoList.Rename(command.Name);
+		var todoList = todoListResult.Value;
+		var renameResult = todoList.Rename(command.Name);
 
-        if (renameResult.IsFailed)
-            return Result.Fail(renameResult.Errors);
+		if (renameResult.IsFailed)
+			return Result.Fail(renameResult.Errors);
 
-        return Result.Ok();
-    }
+		_repository.Update(todoList);
+
+		return Result.Ok();
+	}
 }
