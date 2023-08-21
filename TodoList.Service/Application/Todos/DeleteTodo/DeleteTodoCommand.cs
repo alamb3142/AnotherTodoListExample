dc9edcd69862e4df197e8553926d9fea31b78023
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-21 19:56:13 +0100

    Removes DeleteTodoCommandValidator

--- TodoList.Service/Application/Todos/DeleteTodo/DeleteTodoCommand.cs
+++ TodoList.Service/Application/Todos/DeleteTodo/DeleteTodoCommand.cs
@@ -6,29 +6,21 @@ namespace Application.Todos.DeleteTodo;
 
 public class DeleteTodoCommand : ICommand
 {
-    public int Id { get; init; }
+	public int Id { get; init; }
 }
 
 public class DeleteTodoCommandHandler : ICommandHandler<DeleteTodoCommand>
 {
-    private readonly ITodoRepository repository;
+	private readonly ITodoRepository repository;
 
-    public DeleteTodoCommandHandler(ITodoRepository todoRepository)
-    {
-        repository = todoRepository;
-    }
+	public DeleteTodoCommandHandler(ITodoRepository todoRepository)
+	{
+		repository = todoRepository;
+	}
 
-    public async ValueTask<Unit> Handle(DeleteTodoCommand command, CancellationToken cancellationToken)
-    {
-        await repository.DeleteAsync(command.Id, cancellationToken);
-        return new Unit();
-    }
-}
-
-public class DeleteTodoCommandValidator : AbstractValidator<DeleteTodoCommand>
-{
-    public DeleteTodoCommandValidator()
-    {
-        RuleFor(c => c.Id).NotEmpty();
-    }
+	public async ValueTask<Unit> Handle(DeleteTodoCommand command, CancellationToken cancellationToken)
+	{
+		await repository.DeleteAsync(command.Id, cancellationToken);
+		return new Unit();
+	}
 }
