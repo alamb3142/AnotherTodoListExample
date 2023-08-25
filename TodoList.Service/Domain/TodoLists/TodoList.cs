ecdc86b47ee39ac5b46ac54605fdbf82b61e9707
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-25 14:33:08 +0100

    TodoList.cs formatting

--- TodoList.Service/Domain/TodoLists/TodoList.cs
+++ TodoList.Service/Domain/TodoLists/TodoList.cs
@@ -7,41 +7,41 @@ namespace Domain.TodoLists;
 
 public class TodoList : Entity, IAggregateRoot
 {
-	public Title Title { get; protected set; }
-	public bool Archived { get; protected set; } = false;
-	private readonly List<int> _todoIds;
-	public IReadOnlyCollection<int> TodoIds => _todoIds.AsReadOnly();
+    public Title Title { get; protected set; }
+    public bool Archived { get; protected set; } = false;
+    private readonly List<int> _todoIds;
+    public IReadOnlyCollection<int> TodoIds => _todoIds.AsReadOnly();
 
-	protected TodoList(int id, Title title, List<int> todoIds)
-	{
-		Id = id;
-		Title = title;
-		_todoIds = todoIds;
-	}
+    protected TodoList(int id, Title title, List<int> todoIds)
+    {
+        Id = id;
+        Title = title;
+        _todoIds = todoIds;
+    }
 
-	public static TodoList Create(Title title)
-	{
-		return new TodoList(default, title, new List<int>());
-	}
+    public static TodoList Create(Title title)
+    {
+        return new TodoList(default, title, new List<int>());
+    }
 
-	public Result Rename(string title)
-	{
-		var newTitle = Title.Create(title);
-		if (newTitle.IsFailed)
-			return Result.Fail(newTitle.Errors);
+    public Result Rename(string title)
+    {
+        var newTitle = Title.Create(title);
+        if (newTitle.IsFailed)
+            return Result.Fail(newTitle.Errors);
 
-		Title = newTitle.Value;
-		return Result.Ok();
-	}
+        Title = newTitle.Value;
+        return Result.Ok();
+    }
 
-	public void Archive()
-	{
-		Archived = true;
-		AddDomainEvent(new TodoListArchivedDomainEvent(this));
-	}
+    public void Archive()
+    {
+        Archived = true;
+        AddDomainEvent(new TodoListArchivedDomainEvent(this));
+    }
 
 #nullable disable
-	// to please the EF gods
-	protected TodoList() { }
+    // to please the EF gods
+    protected TodoList() { }
 #nullable enable
 }
