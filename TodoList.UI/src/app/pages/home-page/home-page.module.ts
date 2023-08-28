f6dd879bf1b67dbd36ab1e9acce210bd1d2166c2
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-28 21:23:06 +0100

    Adds nav bar to home page

--- TodoList.UI/src/app/pages/home-page/home-page.module.ts
+++ TodoList.UI/src/app/pages/home-page/home-page.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './home-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListModule } from 'src/app/features/todo-list/todo-list.module';
 import { CoreModule } from 'src/app/core/core.module';
+import { NavBarModule } from 'src/app/features/nav-bar/nav-bar.module';
 
 const routes: Routes = [
 	{
@@ -18,7 +19,8 @@ const routes: Routes = [
 		CommonModule,
 		TodoListModule,
 		RouterModule.forChild(routes),
-		CoreModule
+		CoreModule,
+		NavBarModule
 	],
 	exports: [CoreModule]
 })
