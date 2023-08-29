d5c5236e43e01431c6856d7a449cf895561722fd
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-29 23:04:08 +0100

    Adds provider for http clients

--- TodoList.UI/src/app/app.module.ts
+++ TodoList.UI/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { GrpcCoreModule } from '@ngx-grpc/core';
 import { GrpcWebClientModule } from '@ngx-grpc/grpc-web-client';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HTTP_API_BASE_URL } from './core/http/clients/clients';
+import { HttpClientModule } from '@angular/common/http';
+import { PageSelectorService } from './features/nav-bar/services/page-selector.service';
 
 @NgModule({
 	declarations: [AppComponent],
@@ -16,9 +19,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 		GrpcWebClientModule.forRoot({
 			settings: { host: 'https://localhost:7050' }
 		}),
-		FontAwesomeModule
+		FontAwesomeModule,
+		HttpClientModule
 	],
-	providers: [],
+	providers: [
+		{ provide: HTTP_API_BASE_URL, useValue: 'http://localhost:5167' },
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
