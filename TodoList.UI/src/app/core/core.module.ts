4eaff14b2a653581c9f67f9f39a90e8cc164ad6e
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-30 00:40:21 +0100

    adds todo around API service injection

--- TodoList.UI/src/app/core/core.module.ts
+++ TodoList.UI/src/app/core/core.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 @NgModule({
 	declarations: [],
 	imports: [CommonModule],
+	// TODO: provide interfaced version of 
+	// todo list service that can do either
+	// http or grpc communication
 	providers: []
 })
 export class CoreModule {}
