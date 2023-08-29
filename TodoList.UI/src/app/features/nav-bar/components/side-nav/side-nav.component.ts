7744b3b51cc178376ba21502196b923ca46f10c6
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-29 23:23:02 +0100

    Side Nav component first attempt

--- TodoList.UI/src/app/features/nav-bar/components/side-nav/side-nav.component.ts
+++ TodoList.UI/src/app/features/nav-bar/components/side-nav/side-nav.component.ts
@@ -2,16 +2,39 @@ import { Component } from '@angular/core';
 import { Observable, shareReplay } from 'rxjs';
 import { TodoListSummaryDto } from 'src/app/core/http/clients/clients';
 import { TodoListService } from 'src/app/core/http/todo-list.service';
+import {
+	faHouse,
+	faCalendar,
+	faPlus,
+	faList
+} from '@fortawesome/free-solid-svg-icons';
+import { PageSelectorService } from '../../services/page-selector.service';
 
 @Component({
-  selector: 'app-side-nav',
-  templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+	selector: 'app-side-nav',
+	templateUrl: './side-nav.component.html',
+	styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
-	todoLists$: Observable<TodoListSummaryDto[]>;
+	public todoLists$: Observable<TodoListSummaryDto[]>;
 
-	constructor(todoListService: TodoListService) {
+	public icons = ICONS;
+
+	constructor(
+		todoListService: TodoListService,
+		private readonly pageSelector: PageSelectorService
+	) {
 		this.todoLists$ = todoListService.Get().pipe(shareReplay(1));
 	}
+
+	public isSelected(page: string): boolean {
+		return page === this.pageSelector.currentPage;
+	}
 }
+
+const ICONS = {
+	home: faHouse,
+	calendar: faCalendar,
+	list: faList,
+	plus: faPlus
+};
