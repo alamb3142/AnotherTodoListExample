8f332741f1049f5fe85cd78831a2bfbeb1853a72
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-30 23:09:26 +0100

    adds catch to todo create

--- TodoList.UI/src/app/core/api/http/todo.service.ts
+++ TodoList.UI/src/app/core/api/http/todo.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TodoClient, TodoDto } from './clients/clients';
-import {
-	Observable,
-	Subject,
-	map,
-	startWith,
-	switchMap,
-	tap
-} from 'rxjs';
+import { Observable, Subject, catchError, map, of, startWith, switchMap, tap } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -24,6 +17,7 @@ export class TodoService {
 
 	public create(title: string): Observable<void> {
 		return this.client.todosPost(title).pipe(
+			catchError(() => of(undefined)), // TODO: fix create endpoint
 			map(() => undefined),
 			tap(() => this.refreshSource$.next())
 		);
@@ -66,9 +60,7 @@ export class TodoService {
 		fetchNum?: number
 	): Observable<TodoDto[]> {
 		return this.refresh$.pipe(
-			switchMap(() => {
-				return this.client.filtered(searchTerm, offset, fetchNum);
-			}),
+			switchMap(() => this.client.filtered(searchTerm, offset, fetchNum)),
 			map(result => result.todos ?? [])
 		);
 	}
