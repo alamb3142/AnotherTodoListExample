d7e31bc408a7363e1485e42e3e7c4174edc62dc8
Aaron Lamb <aaronthelamb@outlook.com>
2023-08-30 22:05:24 +0100

    Updates services to return hot observables

--- TodoList.UI/src/app/core/api/http/todo-list.service.ts
+++ TodoList.UI/src/app/core/api/http/todo-list.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import {
 	GetAllTodoListsQuery,
 	TodoListClient,
 	TodoListSummaryDto
 } from './clients/clients';
-import { Observable, map } from 'rxjs';
+import { Observable, Subject, map, startWith, switchMap, tap } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class TodoListService {
-	constructor(private readonly client: TodoListClient) { }
+	private refreshSource$ = new Subject<void>();
+	private refresh$!: Observable<void>;
+
+	constructor(private readonly client: TodoListClient) {
+		this.refresh$ = this.refreshSource$
+			.asObservable()
+			.pipe(startWith(undefined));
+	}
 
 	public Create(name: string): Observable<void> {
 		return this.client.todoListsPost(name).pipe(map(() => undefined));
 	}
 
 	public Get(): Observable<TodoListSummaryDto[]> {
-		return this.client.todoListsGet(new GetAllTodoListsQuery());
+		return this.refresh$.pipe(
+			switchMap(() =>
+				this.client.todoListsGet({} as GetAllTodoListsQuery)
+			)
+		);
 	}
 
 	public Rename(todoListId: number, newName: string): Observable<void> {
-		return this.client.rename(todoListId, newName);
+		return this.client
+			.rename(todoListId, newName)
+			.pipe(tap(() => this.refreshSource$.next()));
 	}
 }
--- TodoList.UI/src/app/core/api/http/todo.service.ts
+++ TodoList.UI/src/app/core/api/http/todo.service.ts
@@ -1,35 +1,63 @@
 import { Injectable } from '@angular/core';
 import { TodoClient, TodoDto } from './clients/clients';
-import { Observable, map } from 'rxjs';
+import {
+	Observable,
+	Subject,
+	map,
+	startWith,
+	switchMap,
+	tap
+} from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class TodoService {
-	constructor(private readonly client: TodoClient) { }
+	private refreshSource$ = new Subject<void>();
+	private refresh$!: Observable<void>;
+
+	constructor(private readonly client: TodoClient) {
+		this.refresh$ = this.refreshSource$
+			.asObservable()
+			.pipe(startWith(undefined));
+	}
 
 	public create(title: string): Observable<void> {
-		return this.client.todosPost(title).pipe(map(() => undefined));
+		return this.client.todosPost(title).pipe(
+			map(() => undefined),
+			tap(() => this.refreshSource$.next())
+		);
 	}
 
 	public complete(todoId: number): Observable<void> {
-		return this.client.complete(todoId);
+		return this.client
+			.complete(todoId)
+			.pipe(tap(() => this.refreshSource$.next()));
 	}
 
 	public rename(todoId: number, newName: string): Observable<void> {
-		return this.client.rename(todoId, newName);
+		return this.client
+			.rename(todoId, newName)
+			.pipe(tap(() => this.refreshSource$.next()));
 	}
 
 	public addToList(todoId: number, todoListId: number): Observable<void> {
-		return this.client.addToList(todoListId, todoId);
+		return this.client
+			.addToList(todoListId, todoId)
+			.pipe(tap(() => this.refreshSource$.next()));
 	}
 
 	public getAll(): Observable<TodoDto[]> {
-		return this.client.todosGet().pipe(map(x => x.todos ?? []));
+		return this.refresh$.pipe(
+			switchMap(() => this.client.todosGet()),
+			map(result => result.todos ?? [])
+		);
 	}
 
 	public getForList(todoListId: number): Observable<TodoDto[]> {
-		return this.client.forList(todoListId);
+		return this.refresh$.pipe(
+			switchMap(() => this.client.forList(todoListId))
+		);
 	}
 
 	public getFiltered(
@@ -37,8 +65,11 @@ export class TodoService {
 		offset?: number,
 		fetchNum?: number
 	): Observable<TodoDto[]> {
-		return this.client
-			.filtered(searchTerm, offset, fetchNum)
-			.pipe(map(x => x.todos ?? []));
+		return this.refresh$.pipe(
+			switchMap(() => {
+				return this.client.filtered(searchTerm, offset, fetchNum);
+			}),
+			map(result => result.todos ?? [])
+		);
 	}
 }
