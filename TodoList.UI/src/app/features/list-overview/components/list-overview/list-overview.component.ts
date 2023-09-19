2c7a50bfb60149feb039f69477ea3017054f129e
Aaron Lamb <aaronthelamb@outlook.com>
2023-09-19 22:58:05 +0100

    Implements list-overview based on todo-list

--- TodoList.UI/src/app/features/list-overview/components/list-overview/list-overview.component.ts
+++ TodoList.UI/src/app/features/list-overview/components/list-overview/list-overview.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl, NonNullableFormBuilder } from '@angular/forms';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { TodoListSummaryDto } from 'src/app/core/api/http/clients/clients';
+import { TodoListService } from 'src/app/core/api/http/todo-list.service';
 
 @Component({
-  selector: 'app-list-overview',
-  templateUrl: './list-overview.component.html',
-  styleUrls: ['./list-overview.component.scss']
+	selector: 'app-list-overview',
+	templateUrl: './list-overview.component.html',
+	styleUrls: ['./list-overview.component.scss']
 })
-export class ListOverviewComponent {
+export class ListOverviewComponent implements OnDestroy {
+	public lists$: Observable<TodoListSummaryDto[]>;
+	public newListControl: FormControl<string> = this.formBuilder.control('');
 
+	private destroy$ = new Subject<void>();
+
+	constructor(
+		private readonly todoListService: TodoListService,
+		private readonly formBuilder: NonNullableFormBuilder
+	) {
+		this.lists$ = this.todoListService.Get();
+	}
+
+	public addTodoList(): void {
+		this.todoListService
+			.Create(this.newListControl.value)
+			.pipe(takeUntil(this.destroy$))
+			.subscribe(() => this.newListControl.reset());
+	}
+
+	public handleKeyPress(event: KeyboardEvent): void {
+		if (event.key == 'Enter') {
+			this.addTodoList();
+		}
+	}
+
+	public ngOnDestroy(): void {
+		this.destroy$.complete();
+	}
 }
