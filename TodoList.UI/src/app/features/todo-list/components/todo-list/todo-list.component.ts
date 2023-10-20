962c63a6cc8431b958ac481c8230a555a0de9343
Aaron Lamb <aaronthelamb@outlook.com>
2023-10-20 16:55:06 +0100

    Adds dyanmic title to list pages

--- TodoList.UI/src/app/features/todo-list/components/todo-list/todo-list.component.ts
+++ TodoList.UI/src/app/features/todo-list/components/todo-list/todo-list.component.ts
@@ -1,7 +1,16 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, NonNullableFormBuilder } from '@angular/forms';
-import { Observable, Subject, map, switchMap, takeUntil, tap } from 'rxjs';
+import {
+	Observable,
+	Subject,
+	combineLatest,
+	map,
+	switchMap,
+	takeUntil,
+	tap
+} from 'rxjs';
 import { TodoDto } from 'src/app/core/api/http/clients/clients';
+import { TodoListService } from 'src/app/core/api/http/todo-list.service';
 import { TodoService } from 'src/app/core/api/http/todo.service';
 
 @Component({
@@ -9,9 +18,9 @@ import { TodoService } from 'src/app/core/api/http/todo.service';
 	templateUrl: './todo-list.component.html',
 	styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnDestroy {
+export class TodoListComponent implements OnInit, OnDestroy {
 	@Input() public todoListId?: Observable<number>;
-	@Input() public title!: string;
+	@Input() public title?: string;
 
 	public newTodo: FormControl<string> = this.formBuilder.control('');
 	public todos$!: Observable<TodoDto[]>;
@@ -21,21 +30,28 @@ export class TodoListComponent implements OnDestroy {
 
 	constructor(
 		private readonly todoService: TodoService,
+		private readonly todoListService: TodoListService,
 		private readonly formBuilder: NonNullableFormBuilder
 	) {}
 
 	public ngOnInit(): void {
-		let todos$: Observable<TodoDto[]>;
-
-		if (!!this.todoListId) {
-			todos$ = this.filterByTodoList(this.todoListId);
-		} else {
-			todos$ = this.todoService.todos$;
-		}
+		let todos$: Observable<TodoDto[]> = !!this.todoListId
+			? this.filterByTodoList(this.todoListId)
+			: this.todoService.todos$;
 
 		this.todos$ = todos$.pipe(
 			map(todos => todos.filter(t => !t.completed))
 		);
+
+		if (!this.title && this.todoListId) {
+			combineLatest([this.todoListId!, this.todoListService.todoLists$])
+				.pipe(takeUntil(this.destroy$))
+				.subscribe(([todoListId, todoLists]) => {
+					this.title = todoLists.find(
+						list => list.id == todoListId
+					)?.title;
+				});
+		}
 	}
 
 	public addTodo(): void {
