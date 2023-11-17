1df4c4e0f821346e12bbc19b8f5f3c668d55ae89
Aaron Lamb <aaronthelamb@outlook.com>
2023-11-17 15:34:16 +0000

    Adds navigation to list-overview

--- TodoList.UI/src/app/features/list-overview/components/list-card/list-card.component.ts
+++ TodoList.UI/src/app/features/list-overview/components/list-card/list-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TodoListSummaryDto } from 'src/app/core/api/http/clients/clients';
+import { Pages } from 'src/app/core/enums/pages';
+import { PageSelectorService } from 'src/app/features/nav-bar/services/page-selector.service';
 
 @Component({
   selector: 'app-list-card',
@@ -8,4 +10,10 @@ import { TodoListSummaryDto } from 'src/app/core/api/http/clients/clients';
 })
 export class ListCardComponent {
 	@Input() todoList!: TodoListSummaryDto;
+
+	constructor(private readonly pageSelector: PageSelectorService) {}
+
+	public navigateToList(): void {
+		this.pageSelector.select(Pages.list, this.todoList.id);
+	}
 }
