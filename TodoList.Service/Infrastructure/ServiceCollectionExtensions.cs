21e977a91e4d0c17ec791500d8aec30e2047cb88
Aaron Lamb <aaronthelamb@outlook.com>
2023-12-28 23:41:06 +0000

    Adds repository to DI + formatting

--- TodoList.Service/Infrastructure/ServiceCollectionExtensions.cs
+++ TodoList.Service/Infrastructure/ServiceCollectionExtensions.cs
@@ -2,11 +2,12 @@ using Application.TodoLists.Common;
 using Application.Todos;
 using Domain.TodoLists;
 using Domain.Todos;
+using Domain.Users;
 using Infrastructure.Common;
 using Infrastructure.TodoLists;
 using Infrastructure.Todos;
+using Infrastructure.Users;
 using Mediator;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,22 +15,23 @@ namespace Infrastructure;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddInfrastructure(this IServiceCollection services, DbSettings settings)
-    {
-        services.AddScoped<ITodoRepository, TodoRepository>();
-        services.AddScoped<ITodoReadRepository, TodoReadRepository>();
-        services.AddScoped<ITodoListRepository, TodoListRepository>();
-        services.AddScoped<ITodoListReadRepository, TodoListReadRepository>();
+	public static void AddInfrastructure(this IServiceCollection services, DbSettings settings)
+	{
+		services.AddScoped<ITodoRepository, TodoRepository>();
+		services.AddScoped<ITodoReadRepository, TodoReadRepository>();
+		services.AddScoped<ITodoListRepository, TodoListRepository>();
+		services.AddScoped<ITodoListReadRepository, TodoListReadRepository>();
+		services.AddScoped<IUserRepository, UserRepository>();
 
-        services.AddSingleton<ISqlConnectionFactory>(
-            new SqlConnectionFactory(settings.ConnectionString)
-        );
+		services.AddSingleton<ISqlConnectionFactory>(
+			new SqlConnectionFactory(settings.ConnectionString)
+		);
 
-        services.AddDbContext<TodoListContext>(options =>
-        {
-            options.UseSqlServer(settings.ConnectionString);
-        });
+		services.AddDbContext<TodoListContext>(options =>
+		{
+			options.UseSqlServer(settings.ConnectionString);
+		});
 
 		services.AddScoped(typeof(IPipelineBehavior<,>), typeof(TransactionBehaviour<,>));
-    }
+	}
 }
