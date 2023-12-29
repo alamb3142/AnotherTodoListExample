4aeb27947c8918c19c56afa7f78bb756e07a8ded
Aaron Lamb <aaronthelamb@outlook.com>
2023-12-29 10:00:13 +0000

    Implements UserRepository

--- TodoList.Service/Infrastructure/Users/UserRepository.cs
+++ TodoList.Service/Infrastructure/Users/UserRepository.cs
@@ -1,27 +1,47 @@
+using Domain.Common.Errors;
 using Domain.Users;
 using FluentResults;
+using Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Users;
 
 public class UserRepository : IUserRepository
 {
-    public void Create(User user)
-    {
-        throw new NotImplementedException();
-    }
+	private TodoListContext _context;
 
-    public Task<Result<User>> GetByIdAsync(int id, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
-
-	public Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken)
+	public UserRepository(TodoListContext context)
 	{
-		throw new NotImplementedException();
+		_context = context;
+	}
+
+	public void Create(User user)
+	{
+		_context.Users.Add(user);
+	}
+
+	public async Task<Result<User>> GetByIdAsync(int id, CancellationToken cancellationToken)
+	{
+		var user = await _context.Users.FindAsync(id, cancellationToken);
+		if (user is null)
+			return Result.Fail(new NotFoundError(id, nameof(User)));
+
+		return Result.Ok(user);
+	}
+
+	public async Task<User?> GetByUsernameAsync(
+		string username,
+		CancellationToken cancellationToken
+	)
+	{
+		return await _context.Users.FirstOrDefaultAsync(
+			u => u.Username == username,
+			cancellationToken
+		);
 	}
 
 	public void Update(User user)
-    {
-        throw new NotImplementedException();
-    }
+	{
+		_context.Users.Update(user);
+	}
 }
