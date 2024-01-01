c0a623c7d9fffae2dd0e13e2112691187f11f147
Aaron Lamb <aaronthelamb@outlook.com>
2024-01-01 21:48:09 +0000

    Fixes Mediator breaking change

--- TodoList.Service/Infrastructure/Common/TransactionBehaviour.cs
+++ TodoList.Service/Infrastructure/Common/TransactionBehaviour.cs
@@ -14,8 +14,8 @@ namespace Infrastructure.Common
 
         public async ValueTask<TResponse> Handle(
             TRequest message,
-            CancellationToken cancellationToken,
-            MessageHandlerDelegate<TRequest, TResponse> next
+            MessageHandlerDelegate<TRequest, TResponse> next,
+            CancellationToken cancellationToken
         )
         {
             var response = await next(message, cancellationToken);
