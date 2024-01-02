d0ffb3a1ba7ea369e6e76054c97b5f353eaf0a21
Aaron Lamb <aaronthelamb@outlook.com>
2024-01-02 20:56:46 +0000

    Adds UserController with register & login

---
+++ TodoList.Service/Api.Http/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+
+using Application.Users.Login;
+using Application.Users.RegisterUser;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Http.Controllers;
+
+[Controller]
+[Route("Users")]
+public class UserController : ControllerBase
+{
+	private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> RegisterUser([FromBody]RegisterUserCommand command, CancellationToken cancellationToken)
+    {
+    	var result = await _mediator.Send(command, cancellationToken);
+    	return this.FromResult(result);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login([FromBody]LoginCommand command, CancellationToken cancellationToken)
+    {
+    	var result = await _mediator.Send(command, cancellationToken);
+    	return this.FromResult(result);
+    }
+}
